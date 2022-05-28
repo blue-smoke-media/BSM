e9ea52bce9d52c83d0a34ebe7084b6751a262e4d
Braden Bell <fish0859@gmail.com>
2022-05-28 15:17:12 -0600

    didn't mean last commit... Changed all src/ to /src/ for MIME type mismatch

--- src/js/jquery.navigation.js
+++ src/js/jquery.navigation.js
@@ -1,4 +1,4 @@
 $(function () {
-    $("header-nav").load("src/html/Navigation/navigation.html")
-    $("header-drop").load("src/html/Navigation/drop-menu.html");
+    $("header-nav").load("/src/html/Navigation/navigation.html")
+    $("header-drop").load("/src/html/Navigation/drop-menu.html");
 })
