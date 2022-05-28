b457f27bbb94d364e576cbc2afa530d7895c867c
Braden Bell <fish0859@gmail.com>
2022-05-28 17:57:35 -0600

    removed /src from all filepaths

--- data/team.js
+++ data/team.js
@@ -1,11 +1,11 @@
 export const team = [{
     name: "Constance Beebe (CoBe) - Co-Founder, Graphic Designer, SEO Specialist",
-    image: "/src/images/CoBe.png",
+    image: "/images/CoBe.png",
     description: "Constance Beebe is a Front-End Web Developer & Graphic Design Artist based out of Knoxville, TN. She is highly accomplished in Search Engine Optimization (SEO), Social Media Marketing and holds a Google Certification in Analytics. She is also skilled in Website Design, Keyword Research, Page Layout, Copywriting and is proficient in the Adobe Creative Suite for Design. She has prior management experience from the many hotels and cabin rental companies, of which she has had the honor of being the general manager. She is a strong engineering professional with a Certificate focused in CSS from the University of Tennessee - Knoxville and a Business Major from Charleston Southern University, in Charleston, SC.",
   },
   {
     name: "Braden Bell - Co-Founder, Fullstack Developer",
-    image: "/src/images/Braden.png",
+    image: "/images/Braden.png",
     description: "I am an avid fly-fisherman with a passion to develop software. The spark that was struck in my youth ignited with a roaring ball of flame. I love to help people solve their problems with a unique solution only I can provide. Change is inevitable, there will always be something that is going to try to hold you back. When there is a new problem, I find a new solution. This is the only way to progress. Today is a new day. I love being a source of color in a binary world!"
   },
   {
--- js/jquery.navigation.js
+++ js/jquery.navigation.js
@@ -1,4 +1,4 @@
 $(function () {
-    $("header-nav").load("/src/html/Navigation/navigation.html")
-    $("header-drop").load("/src/html/Navigation/drop-menu.html");
+    $("header-nav").load("/html/Navigation/navigation.html")
+    $("header-drop").load("/html/Navigation/drop-menu.html");
 })
